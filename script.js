a464989a8177caeca07f8e192a1bf861cc5d21b9
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-18 20:31:55 +0000

    Change Text Inside an Element Using jQuery.
    jQuery has a function called .html() that lets you add HTML tags and text within an element.

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+         $(document).ready(function() {
+         $("button").addClass("animated");
+         $(".btn").addClass("shake");
+         $("#target1").addClass("btn-primary");
+         $("button").removeClass("btn-default");
+         $("#target1").css("color", "red");
+  });
+
+$(document).ready(function(){
+          $("#target1").prop("disabled", true);
+});
